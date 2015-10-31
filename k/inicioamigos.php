f993487a2197a75ab9fa1ecceee47a72aff2fecc
Benjamin <chomi.numb.94@gmail.com>
2015-10-31 17:35:53 -0600

    Agregamos el archivo iniciomaigos

---
+++ k/inicioamigos.php
@@ -0,0 +1,32 @@
+<?php 
+session_start();
+
+include('config.php');
+
+if(!isset($_SESSION['correo'])){
+	print "<meta http-equiv='Refresh' content='1:url=../index'>";
+	die;
+}
+
+$correo = $_SESSION['correo'];
+
+include('lib');
+
+$datos = SUBTRAER_INFORMACION($correo);
+
+$nombre = $datos[0];
+$apellido = $datos[1];
+
+CONECTADO($correo);
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?php print '$nombre $apellido'; ?></title>
+</head>
+<body>
+	<?php print $correo;?>
+</body>
+</html>
