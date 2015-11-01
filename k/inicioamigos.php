e31d3a1acfafe317e29e7b598c00620b87368e2e
Benjamin <chomi.numb.94@gmail.com>
2015-11-01 13:16:18 -0600

    Agregamos el templete que se usara y verificamos algunas cosas de la base de datos

--- k/inicioamigos.php
+++ k/inicioamigos.php
@@ -9,7 +9,7 @@ if(!isset($_SESSION['correo'])){
 $correo = $_SESSION['correo'];
 
 include('lib.php');
-
+include('config.php');
 $datos = SUBTRAER_INFORMACION($correo);
 
 $nombre = $datos[0];
@@ -23,8 +23,20 @@ CONECTADO($correo);
 <head>
 	<meta charset="utf-8">
 	<title><?php print "$nombre $apellido"; ?></title>
+	<link rel="stylesheet" type="text/css" href="estaticos/css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="estaticos/css/estilo.css">
+	<script type="text/javascript" src="estaticos/js/script.js"></script>
 </head>
 <body>
-	<?php print "$correo";?>
+	<?php 
+		$q1 = "select * from comentarios where para='".$correo."' ORDER BY ID DESC limit 0,10";
+		mysql_select_db($dbname);
+		$r1 = mysql_query($q1);
+		while($f1=mysql_fetch_array($r1))
+		{
+			print $f1['de'];
+			print $f1['texto'];
+		}
+	?>
 </body>
 </html>
