98f43ad2d6f77bc97bfc342dd3b102a536f13ad5
Benjamin <chomi.numb.94@gmail.com>
2015-11-01 16:16:14 -0600

    Arreglamos la falta de una letra en el archivo css.php de nuevo

--- estaticos/css/css.php
+++ estaticos/css/css.php
@@ -1,5 +1,5 @@
 <style>
 	.itemPerfil{
-		background: url('../estaticos/img/imgperfil/<?php print $correo;?>');
+		background: url('../estaticos/img/imgperfil/<?php print $correo;?>.jpg');
 	}
 </style>
