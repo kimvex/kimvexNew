f01535cdb8a9c37d0963ceb23ffaa71da41155ef
Benjamin <chomi.numb.94@gmail.com>
2015-11-01 19:02:20 -0600

    Esto esta mejorado y funcional

--- estaticos/js/script.js
+++ estaticos/js/script.js
@@ -4,33 +4,6 @@ document.addEventListener('DOMContentLoaded',function(e){
 		var numero = Math.round(Math.random()*20);
 	}
 	document.getElementById('fondoInicioImg').style.backgroundImage = 'url(estaticos/img/inicio/'+numero+'.jpg)';
-
-	$.fn.scrollf = function(k){
-		var u = $(this);
-
-		$(window).on('scroll', function(){
-			/*estadoTop = window.top;
-			alturaDOM = document.height;
-			alturaVentana = window.height;
-			alturaFinal = alturaDOM - alturaVentana;*/
-			estadoTop = $(window).scrollTop();
-			alturaDOM = $(document).height();
-			alturaVentana = $(window).height();
-			alturaFinal = alturaDOM - alturaVentana;
-
-			if(estadoTop >= alturaFinal){
-				url = k.url;
-				$.get(url,function(datos){
-					document.getElementById('contenedorPub').innerHTML = u + datos;
-				});
-				
-			}
-		});
-	}
-
-	$('#contenedorPub').scrollf({
-		url: 'mas.php'
-	});
 });
 
 $(document).ready(function(){
@@ -39,9 +12,9 @@ $(document).ready(function(){
 		var u = $(this);
 
 		$(window).on('scroll', function(){
-			estadoTop = window.top;
-			alturaDOM = document.height;
-			alturaVentana = window.height;
+			estadoTop = $(window).scrollTop();
+			alturaDOM = $(document).height();
+			alturaVentana = $(window).height();
 			alturaFinal = alturaDOM - alturaVentana;
 
 			if(estadoTop >= alturaFinal){
